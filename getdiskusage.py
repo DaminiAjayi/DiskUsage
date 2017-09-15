f4fb904f785ab8b4af77a01b66164f0155369cc4
Oluwadamini Ajayi <ajayi@Oluwadaminis-MacBook-Pro.local>
2017-09-14 20:58:13 -0500

    Adding command line processing and checks

--- getdiskusage.py
+++ getdiskusage.py
@@ -1,6 +1,7 @@
 import os
 from collections import OrderedDict
 from pprint import pprint
+import sys
 
 def get_usage (mountpoint): 
 
@@ -13,5 +14,17 @@ def get_usage (mountpoint):
 
     return {"files":[ OrderedDict(sorted(files.items(), key=lambda item: item[1], reverse=True))]}
 
+#This enables this script to be called by another script if needed
+if __name__ == '__main__':
+    #Validates that an argurment was given wen calling the function
+    if len (sys.argv) != 2:
+        print ("Usage: getdiskusage.py mountpoint")
+        sys.exit(1)
 
-pprint (get_usage ("/Users/ajayi/Desktop"))
+    #Checks to see if the argument assigned exists
+    if (os.path.isdir(mountpoint)):
+        [print ("Directory does not exist. Please enter a valid directory")]
+        sys.exit(1)
+
+    mountpoint = sys.argv[1]
+    pprint (get_usage (mountpoint))
