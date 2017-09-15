8a8711605e83914e18de6d667e7d96ea4ee3a954
Oluwadamini Ajayi <ajayi@Oluwadaminis-MacBook-Pro.local>
2017-09-15 10:34:16 -0500

    Script complete

--- getdiskusage.py
+++ getdiskusage.py
@@ -10,7 +10,7 @@ from collections import OrderedDict
 from pprint import pprint
 import sys
 
-def get_usage (mountpoint): 
+def get_usage(mountpoint): 
 
     """
     Return all files in mountpoint with corresponding
@@ -19,31 +19,30 @@ def get_usage (mountpoint):
         mountpoint (String): Mount point of the directory
     """
 
-    files = OrderedDict ()
+    files = OrderedDict()
     for root, directory, filenames in os.walk(mountpoint):
-            for filename in filenames: 
-                file = os.path.join(root,filename)
+        for filename in filenames:
+                file = os.path.join(root, filename)
                 size = os.path.getsize(file)
-                files [file] = size
+                files[file] = size
 
     # reverse = True implies descending order
     return {"files":
-            [ OrderedDict(sorted(files.items(),
-                                key=lambda item: item[1],
+            [OrderedDict(sorted(files.items(),
+                            key=lambda item: item[1],
                                 reverse=True))]}
 
 # This enables this script to be called by another script if needed
 if __name__ == '__main__':
-    # Validates that an argurment was given wen calling the function
-    if len (sys.argv) != 2:
-        print ("Usage: getdiskusage.py mountpoint")
+    # Validates that an argurment was given when calling the function
+    if len(sys.argv) != 2:
+        print("Usage: getdiskusage.py mountpoint")
         sys.exit(1)
     mountpoint = sys.argv[1]
 
     # Checks to see if the argument assigned exists
-    if not (os.path.isdir(mountpoint)):
-        print ("Directory does not exist. Please enter a valid directory")
+    if not os.path.isdir(mountpoint):
+        print("Directory does not exist. Please enter a valid directory")
         sys.exit(1)
 
-    
-    pprint (get_usage (mountpoint))
+    pprint(get_usage(mountpoint))
